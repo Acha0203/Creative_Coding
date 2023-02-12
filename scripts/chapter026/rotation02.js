06e1fff5bcbc23af290a50c3433935769d9a6ef2
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-02-13 00:37:29 +0900

    create rotation02-b

--- scripts/chapter026/rotation02.js
+++ scripts/chapter026/rotation02.js
@@ -2,7 +2,6 @@ let angle, r;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB);
   noStroke();
 
   angle = 0;
@@ -10,20 +9,18 @@ function setup() {
 }
 
 function draw() {
-  background(0, 0.03);
+  background(0, 10);
 
   translate(width / 2, height / 2);
-  // clear();
-  rotate(angle);
 
-  // for (let i = 0; i < 300; i++) {
-  const x = cos(angle) * r;
-  const y = sin(angle) * r;
-  fill(x, y, 100);
-  circle(x, y, 10);
+  for (let i = 0; i < 100; i++) {
+    const x = cos(angle) * r;
+    const y = sin(angle) * r;
+    fill(200);
+    circle(x, y, 10);
 
-  angle += 0.1;
+    angle += 0.1;
 
-  r = r > width / 2 ? 0 : r + 1;
-  // }
+    r = r > width / 2 ? 0 : r + 1;
+  }
 }
---
+++ scripts/sketches/rotation02-b.js
@@ -0,0 +1,18 @@
+t = 0;
+draw = () => {
+  t++ || (createCanvas((W = 500), W), noStroke(), (a = r = 0));
+  background(0, 10);
+
+  translate(W / 2, W / 2);
+
+  for (let i = 0; i < 100; i++) {
+    const x = cos(a) * r;
+    const y = sin(a) * r;
+    fill(200);
+    circle(x, y, 5);
+
+    a += 0.25;
+
+    r = r > W / 2 ? 0 : r + 2;
+  }
+};
