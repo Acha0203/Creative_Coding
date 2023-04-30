51042d0f93f7d4e2846a86e22c71413cc1e8302b
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-04-30 13:48:13 +0900

    fix: modify clifford-attractor02

--- scripts/sketches/clifford-attractor02.js
+++ scripts/sketches/clifford-attractor02.js
@@ -1,10 +1,10 @@
 s = 100;
-v = W = 0.1;
+v = w = 0.1;
 (a = -1.5), (b = 1.8), (c = -1.8), (d = -0.4);
 setup = () => {
-  createCanvas((w = 700), w);
+  createCanvas((W = 700), W);
   background('#つぶやきProcessing #p5js');
-  translate(w / 2, w / 2);
+  translate(W / 2, W / 2);
   for (let i = 0; i < 100000; i++) {
     x = sin(a * w) + c * cos(a * v);
     y = sin(b * v) + d * cos(b * w);
