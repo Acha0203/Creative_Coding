a3198e55e2f8d548e365dd8791aef9358e178c84
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-06-05 00:27:02 +0900

    create multiple-particle-system03

--- scripts/sketches/multiple-particle-systems03.js
+++ scripts/sketches/multiple-particle-systems03.js
@@ -3,27 +3,45 @@ let green = 100;
 let blue = 150;
 let greenD = 1;
 let blueD = 1;
+let isFirst = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   systems = [];
+  addNewParticles(width / 2, height / 2);
 }
 
 function draw() {
   background(0);
+
   for (i = 0; i < systems.length; i++) {
     systems[i].run();
     systems[i].addParticle();
+
+    if (systems[i].number <= 0 && systems[i].particles.length <= 0) {
+      systems.splice(i, 1);
+    }
   }
+
   if (systems.length == 0) {
-    fill(255);
-    textAlign(CENTER);
-    textSize(32);
-    text('click mouse to add particle systems', width / 2, height / 2);
+    if (isFirst) {
+      addNewParticles(width / 2, height / 2);
+      isFirst = false;
+    } else {
+      setTimeout(() => {
+        isFirst = true;
+      }, 4000);
+    }
   }
 }
 
 function mousePressed() {
+  if (isHit(mouseX, mouseY)) {
+    addNewParticles(mouseX, mouseY);
+  }
+}
+
+function addNewParticles(x, y) {
   green -= 20 * greenD;
   blue += 20 * blueD;
 
@@ -35,14 +53,39 @@ function mousePressed() {
     blueD *= -1;
   }
 
-  this.p = new ParticleSystem(createVector(mouseX, mouseY), green, blue);
+  let p = new ParticleSystem(createVector(x, y), green, blue);
   systems.push(p);
+}
+
+function isHit(x, y) {
+  for (let i = 0; i < systems.length; i++) {
+    for (let j = 0; j < systems[i].particles.length; j++) {
+      if (
+        x <=
+          systems[i].particles[j].position.x +
+            systems[i].particles[j].size / 2 &&
+        x >=
+          systems[i].particles[j].position.x -
+            systems[i].particles[j].size / 2 &&
+        y <=
+          systems[i].particles[j].position.y +
+            systems[i].particles[j].size / 2 &&
+        y >=
+          systems[i].particles[j].position.y - systems[i].particles[j].size / 2
+      ) {
+        console.log('Hit!');
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 // A simple Particle class
 let Particle = function (position, size) {
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.acceleration = createVector(0, 0.02);
+  this.velocity = createVector(random(-5, 5), random(-5, 5));
   this.position = position.copy();
   this.lifespan = 255.0;
   this.size = size;
@@ -82,12 +125,16 @@ let ParticleSystem = function (position, green, blue) {
   this.particles = [];
   this.green = green;
   this.blue = blue;
+  this.number = 100;
 };
 
 ParticleSystem.prototype.addParticle = function () {
-  p = new Particle(this.origin, random(5, 25));
+  // Add a Particle to the system
+  if (this.number > 0) {
+    p = new Particle(this.origin, random(5, 30));
 
-  this.particles.push(p);
+    this.particles.push(p);
+  }
 };
 
 ParticleSystem.prototype.run = function () {
@@ -96,6 +143,11 @@ ParticleSystem.prototype.run = function () {
     p.run(this.green, this.blue);
     if (p.isDead()) {
       this.particles.splice(i, 1);
+      this.number--;
     }
   }
+};
+
+ParticleSystem.prototype.destroySystem = function () {
+  this.particles = [];
 };
