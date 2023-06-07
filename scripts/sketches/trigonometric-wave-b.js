d63db251d4f98031714e9d329c1af48928d89b2d
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-06-08 08:51:30 +0900

    modify trigonometric-wave

--- scripts/sketches/trigonometric-wave-b.js
+++ scripts/sketches/trigonometric-wave-b.js
@@ -9,10 +9,16 @@ draw = () => {
     fill(233, 100, 100, 0.2);
     circle(
       tan(r * 2 + t) * 300 + W / 5,
-      sin(r * 7 + t + r / noise(2, 6)) * sin(r * 2 + t + r) * 200 +
-        W / 2,
+      sin(r * 7 + t + r / noise(2, 6)) * sin(r * 2 + t + r) * 200 + W / 2,
       20 * sin(r * 9 - t * 9)
     );
   }
 };
 // #つぶやきProcessing #minacoding Day 9
+
+keyPressed = () => {
+  // this will download the first 5 seconds of the animation!
+  if (key === 's') {
+    saveGif('mySketch', 5);
+  }
+};
