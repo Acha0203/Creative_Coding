ddaef6497df7d9fb04700d91da032dcfd4ad36da
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-06-08 07:37:38 +0900

    modify trigonometric-wave

--- scripts/sketches/trigonometric-wave-b.js
+++ scripts/sketches/trigonometric-wave-b.js
@@ -1,23 +1,18 @@
-t = x = y = a = s = A = 0;
-h = 100;
+t = 0;
 draw = () => {
-  t++ || (createCanvas((W = 500), W), noStroke(), (R = random));
-  if (t % 5 == 0) {
-    background(0, 50, 150);
-    for (i = 0; i < 10; i++) {
-      x = 0;
-      y = i * 50;
-      fill(80, R(h, 150), 255, 5);
-      s = 30 + i * 5;
-      while (x < W) {
-        circle(x, y, s);
-        a = R(5, 10);
-        A = t * R(0.01, 0.1) + R(PI);
-        x += a * abs(sin(A));
-        y += a * cos(A);
-        t++;
-      }
-    }
+  t || (createCanvas((W = 720), W), colorMode(HSB), noStroke());
+  t += 0.005;
+  blendMode(BLEND);
+  background(0, 0.1);
+  blendMode(ADD);
+  for (r = 0; r < 15; r += 0.005) {
+    fill(233, 100, 100, 0.2);
+    circle(
+      tan(r * 2 + t) * 300 + W / 5,
+      sin(r * 7 + t + r / noise(2, 6)) * sin(r * 2 + t + r) * 200 +
+        W / 2,
+      20 * sin(r * 9 - t * 9)
+    );
   }
 };
-// #つぶやきProcessing #p5勉強会 #minacoding Day 9
+// #つぶやきProcessing #minacoding Day 9
--- scripts/sketches/trigonometric-wave.js
+++ scripts/sketches/trigonometric-wave.js
@@ -1,31 +1,21 @@
-t = x = y = a = angle = size = 0;
+t = 0;
 
 draw = () => {
-  t++ || (createCanvas(windowWidth, windowHeight), noStroke());
+  t || (createCanvas(windowWidth, windowHeight), colorMode(HSB), noStroke());
+  t += 0.005;
+  blendMode(BLEND);
+  background(0, 0.1);
+  blendMode(ADD);
 
-  if (t % 5 === 0) {
-    for (i = 0; i < height; i++) {
-      fill(30, 30, map(i, 0, height, 100, 200));
-      rect(0, i, width, 1);
-    }
-
-    for (i = 0; i < floor(height / 50); i++) {
-      x = 0;
-      y = i * 50;
-
-      fill(random(50, 100), random(100, 150), 255, random(0, 10));
-      size = random(i * 5, 50 + i * 5);
-
-      while (x < width) {
-        circle(x, y, size);
-
-        a = random(5, 10);
-        angle = t * random(0.01, 0.1) + random(PI);
-        x += a * abs(sin(angle));
-        y += a * cos(angle);
-        t++;
-      }
-    }
+  for (r = 0; r < 15; r += 0.005) {
+    fill(233, 100, 100, 0.2);
+    circle(
+      tan(r * 2 + t) * 300 + width / 5,
+      sin(r * 7 + t + r / noise(2, 6)) * sin(r * 2 + t + r) * 200 +
+        height / 2 +
+        sin(r * 2 + t + r),
+      20 * sin(r * 9 - t * 9)
+    );
   }
 };
-// #p5js #p5勉強会 #minacoding Day 9
+// #p5js #minacoding Day 9
