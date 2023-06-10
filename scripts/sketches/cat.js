a0a87f4b6ba2daee8347a64a22bd788916bf61d4
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-06-10 23:45:23 +0900

    modify cat

--- scripts/sketches/cat.js
+++ scripts/sketches/cat.js
@@ -1,7 +1,5 @@
-let layer1, layer2;
-
-drawEars = (layer, d) => {
-  layer.triangle(
+drawEars = (d) => {
+  triangle(
     width / 2 - 90 * d,
     height / 2 - 180,
     width / 2 - 120 * d,
@@ -11,21 +9,14 @@ drawEars = (layer, d) => {
   );
 };
 
-drawWhiskers = (layer, x, y, d) => {
-  layer.line(x - 100 * d, y + 30, x - 50 * d, y + 50);
-  layer.line(x - 100 * d, y + 60, x - 50 * d, y + 60);
-  layer.line(x - 100 * d, y + 90, x - 50 * d, y + 70);
+drawWhiskers = (x, y, d) => {
+  line(x - 100 * d, y + 30, x - 50 * d, y + 50);
+  line(x - 100 * d, y + 60, x - 50 * d, y + 60);
+  line(x - 100 * d, y + 90, x - 50 * d, y + 70);
 };
 
 setup = () => {
   createCanvas(windowWidth, windowHeight);
-  layer1 = createGraphics(width, height);
-  layer2 = createGraphics(width, height);
-  layer1.background(255);
-  layer1.fill(0);
-  layer1.ellipse(width / 2, height / 2, 300, 250);
-  drawEars(layer1, 1);
-  drawEars(layer1, -1);
 };
 
 draw = () => {
@@ -35,21 +26,24 @@ draw = () => {
   x += floor(movedX / 2);
   y += floor(movedY / 2);
 
-  layer2.clear();
-  layer2.fill(50);
-  layer2.circle(x - 70, y, 50);
-  layer2.circle(x + 70, y, 50);
-  layer2.rect(x - 2, y + 30, 4, 40);
-  layer2.rect(x - 20, y + 70, 40, 4);
-  layer2.ellipse(x, y + 40, 30, 20);
-  layer2.fill(255);
-  layer2.circle(x - 60, y - 10, 9);
-  layer2.circle(x + 80, y - 10, 9);
-  layer2.stroke(50);
-  drawWhiskers(layer2, x, y, 1);
-  drawWhiskers(layer2, x, y, -1);
-
-  image(layer1, 0, 0);
-  image(layer2, 0, 0);
+  clear();
+  background(255);
+  noStroke();
+  fill(0);
+  ellipse(width / 2, height / 2, 300, 250);
+  drawEars(1);
+  drawEars(-1);
+  fill(50);
+  circle(x - 70, y, 50);
+  circle(x + 70, y, 50);
+  rect(x - 2, y + 30, 4, 40);
+  rect(x - 20, y + 70, 40, 4);
+  ellipse(x, y + 40, 30, 20);
+  fill(255);
+  circle(x - 60, y - 10, 9);
+  circle(x + 80, y - 10, 9);
+  stroke(50);
+  drawWhiskers(x, y, 1);
+  drawWhiskers(x, y, -1);
 };
 // #p5js #dailycodingseed #minacoding Day 8
