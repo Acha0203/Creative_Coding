7c501d4631f0c4d8335ce3ec770054c580adafb3
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-06-12 15:46:03 +0900

    optimize text-on-sphere

--- scripts/sketches/texts-on-sphere.js
+++ scripts/sketches/texts-on-sphere.js
@@ -6,12 +6,13 @@ draw = () => {
   t++ ||
     (createCanvas((w = 720), w, WEBGL),
     (m = createGraphics(h, h)),
+    (T = m.text),
     m.background(0));
   m.textSize(35);
   m.fill(r);
   m.translate(10, h / 2);
-  m.text('#つぶやきProcessing', -10, -10);
-  m.text('#minacoding', 20, 40);
+  T('#つぶやきProcessing', -10, -10);
+  T('#minacoding', 20, 40);
   texture(m);
   rotateY(t * -0.01);
   noStroke();
