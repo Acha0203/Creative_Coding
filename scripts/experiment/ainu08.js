b7ded0ae428a2eed540e4eb58b11996f7977e158
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-06-22 14:35:29 +0900

    modify ainu08

--- scripts/experiment/ainu08.js
+++ scripts/experiment/ainu08.js
@@ -8,130 +8,112 @@ function setup() {
 
   layer1.background(0, 50, 255);
   layer2.noStroke();
-  drawPattern3();
+  displayAll();
 }
 
 function draw() {
   clear();
 
   drawBackground(100);
-
   image(layer1, 0, 0);
 
   push();
   imageMode(CENTER);
   translate(width / 2, height / 2);
-  // rotate(frameCount * 0.005);
-  // scale(frameCount * 0.005);
   image(layer2, 0, 0);
   pop();
 }
 
-function drawSpiral(cx, cy, direction) {
-  const spiralCenterX =
-    ((Math.cos(TAU * 2.02) * TAU * 2.02 * height) / 100) * direction;
-  for (let r = 0; r < TAU * 2.02; r += PI / 180) {
-    const p = (r * height) / 100;
-    const x = Math.cos(r) * p * direction + (cx - spiralCenterX);
-    const y = Math.sin(r) * p + cy;
-    layer2.circle(x, y, r + height / 64);
+class AinuPattern {
+  constructor(cx, cy) {
+    this.cx = cx;
+    this.cy = cy;
+  }
+
+  drawSpiral(centerX, maxRadian, direction) {
+    const spiralCenterX = centerX * direction;
+    for (let r = 0; r < maxRadian; r += PI / 180) {
+      const p = (r * height) / 100;
+      const x = Math.cos(r) * p * direction + (this.cx - spiralCenterX);
+      const y = Math.sin(r) * p + this.cy;
+      layer2.circle(x, y, r + height / 64);
+    }
+  }
+
+  drawKnot(size, thickness, spiralRadian, maxRadian, direction) {
+    const spiralCenterY =
+      Math.sin(spiralRadian) * spiralRadian * (height / 100);
+
+    for (let r = 1.5 * PI; r < maxRadian; r += PI / 180) {
+      const x = Math.pow(Math.cos(r), 3) * size * direction + this.cx;
+      const y =
+        Math.pow(Math.sin(r), 3) * size +
+        this.cy +
+        spiralCenterY +
+        height / 520;
+      layer2.circle(x, y, r * thickness);
+    }
+  }
+
+  drawThorn(direction) {
+    const startPointX = Math.cos(2 * PI) * (height / 14) * direction;
+    for (let i = 0; i < height / 2; i += 1) {
+      const r = i * (PI / (height / 4));
+      const x = Math.cos(r) * (height / 14) * direction + this.cx - startPointX;
+      const y = i + Math.cos(r) * (height / 40) + this.cy - height / 150;
+      layer2.circle(x, y, height / 30 - r);
+    }
+  }
+
+  drawPatternA() {
+    const r = TAU * 2.02;
+    this.drawSpiral((Math.cos(r) * r * height) / 100, r, 1);
+    this.drawSpiral((Math.cos(r) * r * height) / 100, r, -1);
+    this.drawKnot(height / 7, height / 260, TAU * 1.75, TAU, 1);
+    this.drawKnot(height / 7, height / 260, TAU * 1.75, TAU, -1);
+    this.drawThorn(1);
+    this.drawThorn(-1);
+  }
+
+  drawPatternB() {
+    this.drawSpiral(
+      (Math.cos(TAU) * TAU * height) / 100 + TAU + height / 256,
+      TAU * 1.72,
+      1
+    );
+    this.drawSpiral(
+      (Math.cos(TAU) * TAU * height) / 100 + TAU + height / 256,
+      TAU * 1.72,
+      -1
+    );
+    this.drawKnot(height / 10, height / 220, TAU * 1.73, (TAU * 35) / 36, 1);
+    this.drawKnot(height / 10, height / 220, TAU * 1.73, (TAU * 35) / 36, -1);
+    this.drawKnot(height / 20, height / 250, TAU * 1.58, (TAU * 35) / 36, 1);
+    this.drawKnot(height / 20, height / 250, TAU * 1.58, (TAU * 35) / 36, -1);
   }
 }
 
-function drawKnot(cx, cy, direction) {
-  const spiralCenterY = Math.sin(TAU * 1.75) * TAU * 1.75 * (height / 100);
-  for (let r = 1.5 * PI; r < TAU; r += PI / 180) {
-    const x = Math.pow(Math.cos(r), 3) * (height / 7) * direction + cx;
-    const y =
-      Math.pow(Math.sin(r), 3) * (height / 7) +
-      cy +
-      spiralCenterY +
-      height / 520;
-    layer2.circle(x, y, r * (height / 260));
-  }
-}
-
-function drawThorn(cx, cy, direction) {
-  const startPointX = Math.cos(2 * PI) * (height / 14) * direction;
-  for (let i = 0; i < height / 2; i += 1) {
-    const r = i * (PI / (height / 4));
-    const x = Math.cos(r) * (height / 14) * direction + cx - startPointX;
-    const y = i + Math.cos(r) * (height / 40) + cy - height / 150;
-    layer2.circle(x, y, height / 30 - r);
-  }
-}
-
-function drawLargeSpiral(cx, cy, direction) {
-  const spiralCenterX =
-    ((Math.cos(TAU) * TAU * height) / 100 + TAU + height / 256) * direction;
-  for (let r = 0; r < TAU * 1.72; r += PI / 180) {
-    const p = (r * height) / 100;
-    const x = Math.cos(r) * p * direction + (cx - spiralCenterX);
-    const y = Math.sin(r) * p + cy;
-    layer2.circle(x, y, r + height / 64);
-  }
-}
-
-function drawKnot2(cx, cy, direction) {
-  let spiralCenterY = Math.sin(TAU * 1.73) * TAU * 1.73 * (height / 100);
-
-  for (let r = 1.5 * PI; r < (TAU * 35) / 36; r += PI / 180) {
-    const x = Math.pow(Math.cos(r), 3) * (height / 10) * direction + cx;
-    const y =
-      Math.pow(Math.sin(r), 3) * (height / 10) +
-      cy +
-      spiralCenterY +
-      height / 520;
-    layer2.circle(x, y, r * (height / 220));
-  }
-
-  spiralCenterY = Math.sin(TAU * 1.58) * TAU * 1.58 * (height / 100);
-
-  for (let r = 1.5 * PI; r < (TAU * 35) / 36; r += PI / 180) {
-    const x = Math.pow(Math.cos(r), 3) * (height / 20) * direction + cx;
-    const y =
-      Math.pow(Math.sin(r), 3) * (height / 20) +
-      cy +
-      spiralCenterY +
-      height / 520;
-    layer2.circle(x, y, r * (height / 250));
-  }
-}
-
-function drawPattern1(x, y) {
-  drawSpiral(x, y, 1);
-  drawSpiral(x, y, -1);
-  drawKnot(x, y, 1);
-  drawKnot(x, y, -1);
-  drawThorn(x, y, 1);
-  drawThorn(x, y, -1);
-}
-
-function drawPattern2(x, y) {
-  drawLargeSpiral(x, y, 1);
-  drawLargeSpiral(x, y, -1);
-  drawKnot2(x, y, 1);
-  drawKnot2(x, y, -1);
-}
-
-function drawPattern3() {
+function displayAll() {
   layer2.background(0);
   layer2.translate(width, height);
   layer2.scale(0.5);
   layer2.rotate(HALF_PI / 2);
   layer2.erase(255);
 
+  const ainuPatternA = new AinuPattern(0, -height * 0.6);
+  const ainuPatternB = new AinuPattern(0, -height * 0.4);
+
   for (let i = 0; i < 4; i++) {
     layer2.rotate(HALF_PI * i);
-    drawPattern2(0, -height * 0.4);
+    ainuPatternA.drawPatternA();
   }
 
-  layer2.scale(0.9);
+  layer2.scale(1.1);
   layer2.rotate(HALF_PI / 2);
 
   for (let i = 0; i < 4; i++) {
     layer2.rotate(HALF_PI * i);
-    drawPattern1(0, -height * 0.6);
+    ainuPatternB.drawPatternB();
   }
 }
 
@@ -148,4 +130,5 @@ function drawBackground(N) {
     }
   }
 }
-// minacoding
+
+// minacoding Day 22: テーマ「人間」5日目
