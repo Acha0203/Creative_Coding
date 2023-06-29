500061bf744163e0ed230996212c83563f775ccf
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-06-29 21:10:20 +0900

    modify minacoding2023

--- scripts/sketches/minacoding2023.js
+++ scripts/sketches/minacoding2023.js
@@ -1,4 +1,5 @@
-const txt = 'minacoding2023';
+const txt1 = 'minacoding2023';
+const txt2 = 'Thank you!';
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -7,21 +8,22 @@ function setup() {
 }
 
 function draw() {
-  const tSize = width / 10;
-  const radius = width / 3;
+  background(0, 0.05);
 
-  background(0, 0.1);
-  fill(255);
-  textSize(tSize);
+  display(txt1, width / 3, width / 10, 0);
+  display(txt2, width / 5, width / 14, 180);
+}
 
+function display(txt, radius, tSize, startHue) {
   for (let i = 0; i < txt.length; i++) {
     const step = TAU / txt.length;
     const x = cos(step * i) * radius + width / 2;
     const y = sin(step * i) * radius + height / 2;
     push();
+    textSize(tSize);
     translate(x, y);
     rotate(step * i + HALF_PI + tan(frameCount / 99));
-    fill((frameCount + i * 26) % 360, 100, 100);
+    fill((startHue + frameCount + i * (360 / txt.length)) % 360, 100, 100);
     text(txt[i], 0, 0);
     pop();
   }
