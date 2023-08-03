a4c9f2cbb1c4ffbf71648e7de23f89e7d015ea0d
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-08-04 06:10:49 +0900

    modify fireworks

--- scripts/sketches/fireworks.js
+++ scripts/sketches/fireworks.js
@@ -1,38 +1,38 @@
-n = 9;
-fireworks = [];
+n = 9
+fireworks = []
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  colorMode(HSB);
-  noStroke();
-  initFireworks();
+  createCanvas(windowWidth, windowHeight)
+  colorMode(HSB)
+  noStroke()
+  initFireworks()
 }
 
 function draw() {
-  background(0, 0.05);
+  background(0, 0.05)
 
   for (let i = 0; i < n; i++) {
     for (let r = 0; r < TAU; r += PI / n) {
-      let length = random(frameCount % fireworks[i].size);
-      x = cos(r) * length + fireworks[i].x;
-      y = sin(r) * length + fireworks[i].y;
-      fill((frameCount + length) % 360, 80, 90);
-      circle(x, y, length / 20);
+      let length = random(frameCount % fireworks[i].size)
+      x = cos(r) * length + fireworks[i].x
+      y = sin(r) * length + fireworks[i].y
+      fill((frameCount + length) % 360, 80, 90)
+      circle(x, y, length / 20)
     }
   }
 
-  if (frameCount % 90 <= 0) initFireworks();
+  if (frameCount % 90 <= 0) initFireworks()
 }
 
 function initFireworks() {
-  fireworks = [];
+  fireworks = []
 
   for (let i = 0; i < n; i++) {
     fireworks.push({
       x: random(width),
       y: random(height),
-      size: random(90, 500),
-    });
+      size: random(width / 10, width / 2),
+    })
   }
 }
 
