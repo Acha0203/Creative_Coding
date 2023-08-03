ee48c2931454ef3df4da4715f548075ba68c0f60
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2023-08-03 15:56:51 +0900

    modify fireworks02

--- scripts/sketches/fireworks02.js
+++ scripts/sketches/fireworks02.js
@@ -66,12 +66,9 @@ class Firework {
   }
 
   run() {
-    for (let i = this.particles.length - 1; i >= 0; i--) {
-      let p = this.particles[i]
-      p.run()
-      if (p.isDead()) {
-        this.particles.splice(i, 1)
-      }
+    this.particles = this.particles.filter((p) => !p.isDead())
+    for (let i = 0; i < this.particles.length; i++) {
+      this.particles[i].run()
     }
   }
 }
