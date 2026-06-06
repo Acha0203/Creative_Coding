b6238d7f458eee38ae14d6b28c8738d09e141bcc
Atsuko Ikeda <ikedaatsuko@PrimaMachinaiMac2023.local>
2026-06-06 13:49:22 +0900

    modify ocean-current

--- scripts/esquisse.js
+++ scripts/esquisse.js
@@ -1,6 +1,4 @@
-const frequency = 8;
-const volume = 0.3;
-const interval = 40;
+let t = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -9,19 +7,25 @@ function setup() {
 }
 
 function draw() {
-  background(0, 0.05);
+  let w = width / 2;
+  let h = height / 2;
+  let fishSize = Math.max(width, height) / 30;
 
-  const numPoints = width / interval;
+  t += 0.01;
+  blendMode(BLEND);
+  background(240, 255, 20, 0.1);
+  blendMode(ADD);
 
-  for (let i = 1; i < numPoints; i++) {
-    const x = (frameCount + i * interval) % width;
-    let y =
-      height / 2 + height * volume * sin(((x + i / noise(interval)) * frequency * TAU) / width);
-
-    fill(frameCount % (i * interval), 100, 100, 0.2);
-    circle(x, y, interval * sin(i));
+  for (let r = 0; r < fishSize; r += 0.1) {
+    fill(200 + r, 8 * r, 8 * r, 0.2);
+    ellipse(
+      tan(r * 2 + t) * w + w,
+      sin(t + r / noise(3, 9)) * sin(r * 5 + t) * h + h,
+      fishSize,
+      fishSize / 3,
+    );
   }
 }
 
-// #minacoding 2026 June 6th, Place
-// I coded this on p5.js Web Editor.
+// #minacoding 2026 June 5th, Sea
+// An image of an ocean current or a school of fish.
--- scripts/sketches/ocean-current.js
+++ scripts/sketches/ocean-current.js
@@ -9,15 +9,21 @@ function setup() {
 function draw() {
   let w = width / 2;
   let h = height / 2;
+  let fishSize = Math.max(width, height) / 30;
 
   t += 0.01;
   blendMode(BLEND);
   background(240, 255, 20, 0.1);
   blendMode(ADD);
 
-  for (let r = 0; r < 30; r += 0.1) {
+  for (let r = 0; r < fishSize; r += 0.1) {
     fill(200 + r, 8 * r, 8 * r, 0.2);
-    ellipse(tan(r * 2 + t) * w + w, sin(t + r / noise(3, 9)) * sin(r * 5 + t) * h + h, 30, 9);
+    ellipse(
+      tan(r * 2 + t) * w + w,
+      sin(t + r / noise(3, 9)) * sin(r * 5 + t) * h + h,
+      fishSize,
+      fishSize / 3,
+    );
   }
 }
 
